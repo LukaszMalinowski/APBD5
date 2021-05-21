52a90fb15fc3b8672468a3628826d35ba5297964
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-05-21 10:25:31 +0200

    Fix - changed int to DateTime in ClientTrip.cs

--- Models/ClientTrip.cs
+++ Models/ClientTrip.cs
@@ -9,8 +9,8 @@ namespace cwiczenia5_zen_s19743.Models
     {
         public int IdClient { get; set; }
         public int IdTrip { get; set; }
-        public int RegisteredAt { get; set; }
-        public int? PaymentDate { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public DateTime? PaymentDate { get; set; }
 
         public virtual Client IdClientNavigation { get; set; }
         public virtual Trip IdTripNavigation { get; set; }
